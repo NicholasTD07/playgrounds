9079a6faccbc65300fa7043166b9ff180d328cd1
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-02-07 09:21:40 +1000

    add function: contains_commit
    Use this function to tell whether a branch contains a commit or not.

--- shell/git/git.sh
+++ shell/git/git.sh
@@ -9,5 +9,17 @@ current_branch() {
     git rev-parse --abbrev-ref HEAD
 }
 
+contains_commit() {
+    # example usage: contains_commit develop master
+    #
+    # to determine whether 'develop' contains 'master'
+    # -> whether 'master' is an ancester of 'develop'
+
+    local subject=$1
+    local commit=$2
+
+    git merge-base --is-ancestor $commit $subject
+}
+
 }
 
