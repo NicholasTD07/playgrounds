bda192784f9c16c04291c0c39b43e5d13c092d79
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-02-07 09:20:54 +1000

    add function: current_branch

--- shell/git/git.sh
+++ shell/git/git.sh
@@ -3,5 +3,11 @@
 local_branches() {
     git for-each-ref refs/heads --format='%(refname:short)'
 }
+
+current_branch() {
+    # only works on local branches
+    git rev-parse --abbrev-ref HEAD
+}
+
 }
 
