dc69c3da725b7f1db922af8ca4c5647be223e014
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-02-09 13:18:59 +1000

    [shell-git] refactor merge-with-everything-else.rb

--- shell/git/merge-with-everything-else.rb
+++ shell/git/merge-with-everything-else.rb
@@ -1,5 +1,25 @@
 #!/usr/bin/env ruby
 
+def main
+  other_branches = local_braches.select { |b| b != current_branch }
+
+  puts "Merge with #{other_branches.join(', ')}? y[es]/n[o]"
+  do_merge = get_boolean_input
+
+  exit unless do_merge
+
+  merged_branches = []
+  for branch in other_branches
+    `git merge #{branch}`
+    unless $?.success?
+      failed_when_merging branch
+      merged_with merged_branches
+      return
+    end
+    merged_branches << branch
+  end
+end
+
 def local_braches
   `git for-each-ref refs/heads --format='%(refname:short)'`.split
 end
@@ -8,22 +28,23 @@ def current_branch
   `git rev-parse --abbrev-ref HEAD`.strip
 end
 
-other_branches = local_braches.select { |b| b != current_branch }
-
-puts "Merge with #{other_branches.join(', ')}? y[es]/n[o]"
-
-case gets.strip.downcase
-when 'y', 'yes'
-  do_merge = true
-when 'n', 'no'
-  do_merge = false
+def get_boolean_input
+  case gets.strip.downcase
+  when 'y', 'yes'
+    return true
+  when 'n', 'no'
+    return false
+  end
 end
 
-exit unless do_merge
-
-for branch in other_branches
-  `git merge #{branch}`
-  exit unless $?.success?
-  puts "Merged with #{branch}"
+def failed_when_merging(branch)
+  puts "Failed when merging with #{branch}"
 end
+
+def merged_with(branches)
+  puts "Merged with #{branches.join(', ')}"
+end
+
+
+main
 
