50f85dc0c259a9704d4b72bad2db7afc85581c3a
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-02-16 11:32:13 +1000

    [user-group] add context and tag for User's spec

--- ruby/rails/user-group/spec/models/user_spec.rb
+++ ruby/rails/user-group/spec/models/user_spec.rb
@@ -1,15 +1,18 @@
 require_relative '../rails_helper'
 
 describe User do
-  let(:bob) { FactoryGirl.create(:user) }
-  let(:alice) { FactoryGirl.create(:user) }
-  it 'has people added to its relationships' do
-    bob.relationships << Relationship.new(name: 'friends',
-                                      user_id: bob.id,
-                                      other_user_id: alice.id)
+  context 'Relationship', :model  => true do
+    # skip this spec by running
+    # `rspec spec --tag ~model`
+    let(:bob) { FactoryGirl.create(:user) }
+    let(:alice) { FactoryGirl.create(:user) }
 
-    expect(bob.related_people).to include alice
+    it 'has people added to its relationships' do
+      bob.relationships << Relationship.new(name: 'friends',
+                                        user_id: bob.id,
+                                        other_user_id: alice.id)
+
+      expect(bob.related_people).to include alice
+    end
   end
-
-  it 'maybe this should be in Relationship?'
 end
