f0244c50fd04be28e43808c6c7c9aeddb29dac72
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-03-14 09:44:53 +0800

    [ruby-misc] move specs into a spec file

--- ruby/misc/investment-calculator.rb
+++ ruby/misc/investment-calculator.rb
@@ -21,30 +21,3 @@ class InvestmentCalculator
   end
 
 end
-
-describe InvestmentCalculator do
-  let!(:yearly_investment) { 1500 }
-  let!(:interest) { 0.25 }
-  subject { InvestmentCalculator.new(yearly_investment, interest) }
-
-  context 'when it didnt start running' do
-    it 'should have no value' do
-      expect(subject.profit_for_years 0).to eq 0
-    end
-  end
-
-  context 'when it only runs for 1 year' do
-    it 'should have base investment + 1 years interest' do
-      expect(subject.profit_for_years 1).to eq yearly_investment * (1 + interest)
-    end
-  end
-
-  context 'when it only runs for 2 year' do
-    it 'should have complex profit' do
-      expect(subject.profit_for_years 2).to eq (yearly_investment * (1 + interest) ** 2 + yearly_investment * (1 + interest))
-    end
-  end
-
-end
-
-# p InvestmentCalculator.new(18000, 10, 0.25).calculate
