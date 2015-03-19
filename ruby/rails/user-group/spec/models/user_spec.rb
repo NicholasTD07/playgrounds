a4bb11f7c80316a31ab58600f64fe842139a05df
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-03-19 16:03:36 +1000

    [user-group] add `touch` option in Relationship

--- ruby/rails/user-group/spec/models/user_spec.rb
+++ ruby/rails/user-group/spec/models/user_spec.rb
@@ -14,5 +14,20 @@ describe User do
 
       expect(bob.related_people).to include alice
     end
+
+    it 'should not change' do
+      i = 0
+      expect {  }.not_to change { i }
+    end
+
+    it 'does change users updated_at' do
+      expect do
+        bob.relationships << Relationship.new(name: 'friends',
+                                              user_id: bob.id,
+                                              other_user_id: alice.id)
+      end.to change { bob.updated_at }
+
+    end
+
   end
 end
