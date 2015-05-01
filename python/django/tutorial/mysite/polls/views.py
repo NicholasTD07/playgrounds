62220ea0e2b3260602c64b59d27e7300c250f836
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-05-01 22:34:59 +1000

    [app] url, view, template for polls/detail

--- python/django/tutorial/mysite/polls/views.py
+++ python/django/tutorial/mysite/polls/views.py
@@ -8,3 +8,9 @@ def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_questions': latest_questions}
     return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return render(
+            request, 'polls/detail.html',
+            {'question': get_object_or_404(Question, pk=question_id)}
+           )
