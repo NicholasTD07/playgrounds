39829b03664ac63de888cdb449e1520cf7ae3f93
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-05-02 07:56:04 +1000

    [app] can vote a poll and see results of a poll

--- python/django/tutorial/mysite/polls/views.py
+++ python/django/tutorial/mysite/polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 
 # Create your views here.
@@ -13,3 +15,25 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {
         'question': get_object_or_404(Question, pk=question_id)
         })
+
+def results(request, question_id):
+    return render(request, 'polls/results.html', {
+            'question': get_object_or_404(Question, pk=question_id)
+            })
+
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+            })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # return redirect after successfully dealing with post, avoid double post
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
